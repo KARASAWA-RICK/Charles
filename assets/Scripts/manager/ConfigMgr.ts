32779da6087f4833d1c8181bf2b4bff5d0aeb2f4
Rick <49814330+KARASAWA-RICK@users.noreply.github.com>
2023-08-13 16:40:27 +0800

    关于Excel导表的注释

--- assets/Scripts/manager/ConfigMgr.ts
+++ assets/Scripts/manager/ConfigMgr.ts
@@ -1,79 +1,83 @@
-import { _decorator, Component, JsonAsset, Node } from 'cc';
+import { _decorator, Component, JsonAsset, Node } from "cc";
 const { ccclass, property } = _decorator;
 
-@ccclass('ConfigMgr')
-export class ConfigMgr  {
-    private static _ins: ConfigMgr = null!;
+@ccclass("ConfigMgr")
+export class ConfigMgr {
+  /**
+   *单例
+   *
+   * @private
+   * @static
+   * @type {ConfigMgr}
+   * @memberof ConfigMgr
+   */
+  private static _ins: ConfigMgr = null!;
+  public static get ins() {
+    if (!this._ins) {
+      this._ins = new ConfigMgr();
+    }
+    return this._ins;
+  }
 
-    public static get ins() {
-        if (!this._ins) {
-            this._ins = new ConfigMgr();
+  /**
+   *存所有Json配置表的对象
+   *不直接用，给addTable方法用
+   * @private
+   * @type {*}
+   * @memberof ConfigMgr
+   */
+  private _dataTables: any = {};
 
-        }
-
-        return this._ins;
+  /**
+   *添加Json配置表的方法
+   *不直接用，给ResMgr用
+   * @param {string} tableName 表名作键
+   * @param {*} tableContent 一张表的Json对象
+   * @return {*}
+   * @memberof ConfigMgr
+   */
+  addTable(tableName: string, tableContent: any) {
+    if (this._dataTables[tableName]) {
+      return;
     }
 
-    private _dataTables:any = {};
-
-
-
-    addTable (tableName:string, tableContent:any) {
-        if (this._dataTables[tableName]) {
-            return;
-        }
-
-        let dict={}
-        let num=tableContent.data.length;
-        for (let i = 0; i < num; i++) {
-            let data=tableContent.data[i];
-            dict[Number(data.id)]=data;//通过id保存
-
-            // console.log("addTable==>"+data.id)
-        }
-
-        this._dataTables[tableName] = dict;
-
-
+    //用于存放单个Json配置表对象
+    let dict = {};
+    let num = tableContent.data.length;
+    //遍历Json对象数组
+    for (let i = 0; i < num; i++) {
+      //获取每个Json元素（每行）
+      let data = tableContent.data[i];
+      //存放每行的Json对象（id做键，Json对象作值）
+      dict[Number(data.id)] = data; //通过id保存
     }
 
+    this._dataTables[tableName] = dict;
+  }
 
-    /**
-     * 根据表名获取表的所有内容
-     * @param {string} tableName  表名
-     * @returns {object} 表内容
-     */
-    getTable (tableName:string) {
-        return this._dataTables[tableName];
+  /**
+   * 根据表名获取单个Json配置表对象
+   * @param {string} tableName  表名
+   * @returns {object} 单个Json配置表对象
+   */
+  getTable(tableName: string) {
+    return this._dataTables[tableName];
+  }
+
+  /**
+   * 根据表名和id获取一行Json对象
+   * @param {string} tableName 表名
+   * @param {number} key 一行Json对象的id
+   * @returns {Object} 一行Json对象
+   */
+  queryOne(tableName: string, key: number) {
+    var table = this.getTable(tableName);
+    if (!table) {
+      return null;
     }
 
-    /**
-     * 查询一条表内容
-     * @param {string} tableName 表名
-     * @param {number} key 唯一ID
-     * @returns {Object} 一条表内容
-     */
-    queryOne (tableName:string, key:number) {
-        var table = this.getTable(tableName);
-        if (!table) {
-            return null;
-        }
+    console.log("queryOne==>>" + tableName + "," + key);
 
-        console.log("queryOne==>>"+tableName+","+key)
-        
-        return table[key];
-        
-    }
-
-    /**
-     * 根据ID查询一条表内容
-     * @param {string}tableName 表名
-     * @param {string}ID
-     * @returns {Object} 一条表内容
-     */
-    queryByID (tableName:string, ID:string) {
-        //@ts-ignore
-        return this.queryOne(tableName, ID);
-    }
+    return table[key];
+  }
 }
-
